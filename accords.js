4089b1fd5e3df99e15dea6f4ac6c0369afac72d3
Mikhail Ivanov <55839915+midevelop@users.noreply.github.com>
2019-12-08 00:40:40 +0300

    feat(accords): uuu

--- accords.js
+++ accords.js
@@ -4,11 +4,12 @@ const accords = ['A', 'A#', 'H', 'C', 'C#', 'D', "D#", "E", "F", "F#", 'G', 'G#'
 
 console.log(accords)
 
-let findAccord = (inputNote = 'C', major = true) => {
-  let index = accords.findIndex(note => note === inputNote)
+let findAccordByNote = (inputNote = 'C', major = true) => {
+  let index = accords.findIndex(note => note.toLowerCase() === inputNote.toLowerCase())
+  if (index === -1) return []
   let second = (index + (major ? 4 : 3)) % 12
   let third = (index + 7) % 12
   return [accords[index], accords[second], accords[third]]
 }
 
-console.log(findAccord('G', true))
+console.log(findAccordByNote('cdf', true))
